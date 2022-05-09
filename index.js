e424f4eb380da736205167b252b176babd202ba2
Kevin Nielsen <knielsen24@gmail.com>
2022-05-09 15:16:42 -0700

    click function with target

--- index.js
+++ index.js
@@ -6,64 +6,57 @@ const getRandomInt = (max) => {
     return Math.floor(Math.random() * max)
 }
 
-// potato
 
 fetch(url)
-.then(r => r.json())
-.then(gameArray => {
-    renderMain(gameArray)
-    renderList(gameArray)
-    randomGameButton(gameArray)
-})
-    let name = document.createElement('h3')
-    let image = document.createElement('img')
-    let price = document.createElement('h3')
-    let sale = document.createElement('h3')
+    .then(r => r.json())
+    .then(gameArray => {
+        renderMain(gameArray)
+        renderList(gameArray)
+        randomGameButton(gameArray)
+    })
+
+let name = document.createElement('h3')
+let image = document.createElement('img')
+let price = document.createElement('h3')
+let sale = document.createElement('h3')
 
 const renderMain = (gameArray) => {
     
     let r = getRandomInt(gameArray.length - 1)
 
-    
-
     const {title, thumb, normalPrice, salePrice} = gameArray[r]
     
     name.innerText = title.toString()
     image.src = thumb.toString()
-    price.innerText = normalPrice
-    sale.innerText = salePrice
+    price.innerText = `msrp $${normalPrice}`
+    sale.innerText = `Current Price  $${salePrice}`
 
     mainCard.append(name, image, price, sale)
 }
 
 
-const renderList = (gameArray) => {
-
-    gameArray.forEach(gameObj => {
-    
-    const list = document.getElementById('list')
-    const name = document.createElement('li')
-
-    name.textContent = gameObj.title
-    
-    //I'm here to be worked on! 
-    //I should render the game that gets clicked
-    name.addEventListener('click', (e) => {
-        e.preventDefault()
-    });
-    list.append(name)
+const clickTitle = (titleName) => {
+    titleName.addEventListener('click', (e) => {
+        console.log(e.target.title.value)
+        e.target.title.value
     });
 }
+
+const renderList = (gameArray) => {
+    gameArray.forEach(gameObj => {
+        const list = document.getElementById('list')
+        const name = document.createElement('li')
+        name.textContent = gameObj.title
+        list.append(name)
+        clickTitle(name)
+    });
+}
+
 const randomGameButton = (gameArray) => {
     const randomGame = document.createElement('button')
     randomGame.innerText = 'Click Me For A Random Game';
     document.getElementById('random-game').append(randomGame)
     randomGame.addEventListener('click', (e) => {
-       
-       name.remove()
-       image.remove()
-       price.remove()
-       sale.remove()
        renderMain(gameArray)
     })
 }
